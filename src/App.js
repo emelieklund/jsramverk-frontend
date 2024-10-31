a9679f82cc1a7586f37de6982ec609e64317b278
emelieklund <emelieeeklund@gmail.com>
2024-10-31 22:00:53 +0100

    Added routes for users and create users

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Routes, Route, HashRouter } from "react-router-dom";
 import Doc from './pages/Doc.js';
+import CreateUser from './pages/CreateUser.js';
 import Home from './pages/Home.js';
+import Users from './pages/Users.js';
 import NoPage from './pages/NoPage.js';
 import './style/App.css';
 
@@ -11,6 +13,8 @@ function App() {
             <Routes>
                 <Route index element={<Home />} />
                 <Route path="/doc/:id" element={<Doc />} />
+                <Route path="/create-user" element={<CreateUser />} />
+                <Route path="/users" element={<Users />} />
                 <Route path="*" element={<NoPage />} />
             </Routes>
         </HashRouter>
