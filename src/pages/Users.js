5e3fbaefbf8f96da382589ad10aeb2929b903b20
emelieklund <emelieeeklund@gmail.com>
2024-11-01 22:55:16 +0100

    Added more detailed information. Temporary component

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 
 import '../style/Users.css';
 
@@ -26,27 +29,42 @@ function Users() {
         getUsers();
     }, [])
 
+    // Delete from backend
+    const handleDelete = async (email) => {
+        console.log(email)
+        await axios.post(`${AZURE}/users/delete/${email}`);
+
+        //window.location.reload(false);
+    }
+
+
     return (
-        <div className="table-div" >
-            <Table className="table" >
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Username/Email</TableCell>
-                        <TableCell>Password</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {users.map((data, i) => (
-                        <TableRow
-                            key={i}
-                            className="row"
-                        >
-                            <TableCell>{data.email}</TableCell>
-                            <TableCell>{data.password}</TableCell>
+        <div className="home-div" >
+            <div className="table-div" >
+                <Table className="table" >
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Username/Email</TableCell>
+                            <TableCell>Password</TableCell>
+                            <TableCell>Created</TableCell>
+                            <TableCell></TableCell>
                         </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
+                    </TableHead>
+                    <TableBody>
+                        {users.map((data, i) => (
+                            <TableRow
+                                key={i}
+                                className="row"
+                            >
+                                <TableCell>{data.email}</TableCell>
+                                <TableCell>{data.password}</TableCell>
+                                <TableCell>{data.account_created}</TableCell>
+                                <TableCell align="right"><FontAwesomeIcon icon={faTrashCan} id="trash-icon" onClick={() => handleDelete(data.email)} /></TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </div>
         </div>
     );
 }
